da19698fcface12694a4768e9997c9da73731c67
roylin <1002591652@qq.com>
2019-07-17 15:07:55 +0800

    feat: add typeorm

--- service/src/app.module.ts
+++ service/src/app.module.ts
@@ -1,9 +1,21 @@
+import { resolve } from 'path';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RenderModule } from 'nest-next';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [RenderModule],
+  imports: [
+    RenderModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sql',
+      logging: true,
+      dropSchema: true,
+      synchronize: true,
+      entities: [resolve('./**/*.model.js')]
+    })
+  ],
   controllers: [AppController]
 })
 export class AppModule { }
