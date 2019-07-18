79a0e6f44b9bc6b5448c4a0fc6cdd0b9b94089b7
roylin <1002591652@qq.com>
2019-07-18 12:14:05 +0800

    refactor: lerna

--- service/packages/logger/context-trace.ts
+++
@@ -1,8 +0,0 @@
-export class ContextTrace {
-    constructor(
-        public readonly context: string,
-        public readonly path?: string,
-        public readonly lineNumber?: number,
-        public readonly columnNumber?: number
-    ) { }
-}
