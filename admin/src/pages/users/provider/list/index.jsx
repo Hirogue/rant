98472f430a3532341991dcec0a0aebfdfd53860f
Hirogue <314367747@qq.com>
2019-08-30 23:17:00 +0800

    add users page

---
+++ admin/src/pages/users/provider/list/index.jsx
@@ -0,0 +1,262 @@
+import StandardActions from '@/components/StandardActions';
+import StandardConfirm from '@/components/StandardConfirm';
+import StandardRow from '@/components/StandardRow';
+import StandardTable from '@/components/StandardTable';
+import { M_DELETE_USER, Q_GET_USERS } from '@/gql';
+import { canCreateAny, canDeleteAny, canReadAny, canUpdateAny } from '@/utils/access-control';
+import { IdentityEnum, UserStatusEnum } from '@/utils/enum';
+import {
+  buildingQuery,
+  paramsAuth,
+  UserLevelMaps,
+  UserStatusMaps,
+  UserTypeMaps,
+} from '@/utils/global';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { useApolloClient, useQuery } from '@apollo/react-hooks';
+import { CondOperator } from '@nestjsx/crud-request';
+import { Affix, Col, Divider, message, Popconfirm, Row, Skeleton } from 'antd';
+import moment from 'moment';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Link, router } from 'umi';
+import { M_APPROVAL_USER } from '../../gql';
+
+const PATH = '/users/provider';
+const AUTH_RESOURCE = '/user/provider';
+
+export default () => {
+  const defaultFilter = [
+    { field: 'identity', operator: CondOperator.EQUALS, value: IdentityEnum.PROVIDER },
+  ];
+  const defaultVariables = {
+    page: 0,
+    limit: 10,
+    filter: defaultFilter,
+    sort: [{ field: 'create_at', order: 'DESC' }],
+  };
+  const [variables, setVariables] = useState(defaultVariables);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [current, setCurrent] = useState(null);
+
+  const client = useApolloClient();
+
+  const { loading, data, refetch } = useQuery(Q_GET_USERS, {
+    notifyOnNetworkStatusChange: true,
+    variables: {
+      queryString: buildingQuery(defaultVariables),
+    },
+  });
+
+  useEffect(() => {
+    const queryString = buildingQuery(variables);
+
+    refetch({ queryString });
+  }, [variables]);
+
+  const { queryUser, roles } = data;
+
+  if (!queryUser) return <Skeleton loading={loading} active avatar />;
+
+  const renderActions = record => {
+    if (UserStatusEnum.PENDING === record.status && canUpdateAny(AUTH_RESOURCE)) {
+      return (
+        <Fragment>
+          <Popconfirm
+            title="确定要审核吗?"
+            onConfirm={() => {
+              client.mutate({
+                mutation: M_APPROVAL_USER,
+                variables: {
+                  data: {
+                    id: record.id,
+                    status: UserStatusEnum.CHECKED,
+                  },
+                },
+                update: (cache, { data }) => {
+                  if (data.approvalUser) {
+                    message.success('操作成功');
+                    refetch();
+                  }
+                },
+              });
+            }}
+          >
+            <a href="#">[审核]</a>
+          </Popconfirm>
+          <Divider type="vertical" />
+          <a
+            href="javascript:;"
+            onClick={() => {
+              setCurrent(record);
+              setVisible(true);
+            }}
+          >
+            [驳回]
+          </a>
+        </Fragment>
+      );
+    } else {
+      return null;
+    }
+  };
+
+  const dataSource = queryUser.data;
+  const total = queryUser.total;
+
+  const columns = [
+    {
+      title: '详情',
+
+      render: (val, row) =>
+        canUpdateAny(AUTH_RESOURCE) ? <Link to={`${PATH}/detail/${row.id}`}>详情</Link> : '--',
+    },
+    {
+      title: '账户',
+      dataIndex: 'account',
+      search: true,
+    },
+    {
+      title: '姓名',
+      dataIndex: 'realname',
+      search: true,
+    },
+    {
+      title: '手机号',
+      dataIndex: 'phone',
+      search: true,
+    },
+    {
+      title: '地区',
+      dataIndex: 'area.title',
+      render: (val, record) => (record.area ? record.area.title : ''),
+      search: true,
+    },
+    {
+      title: '类型',
+      dataIndex: 'type',
+      render: val => UserTypeMaps[val],
+      filters: Object.keys(UserTypeMaps).map(key => ({ text: UserTypeMaps[key], value: key })),
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      render: val => UserStatusMaps[val],
+      filters: Object.keys(UserStatusMaps).map(key => ({ text: UserStatusMaps[key], value: key })),
+    },
+    {
+      title: '等级',
+      dataIndex: 'vip',
+      render: val => UserLevelMaps[val],
+      filters: Object.keys(UserLevelMaps).map(key => ({ text: UserLevelMaps[key], value: key })),
+    },
+    {
+      title: '注册时间',
+      dataIndex: 'create_at',
+      render: val => moment(val).format('YYYY-MM-DD HH:mm:ss'),
+      sorter: true,
+    },
+    {
+      title: '操作',
+      render: (val, record) => renderActions(record),
+    },
+  ];
+
+  const pagination = {
+    size: 'small',
+    total,
+    current: variables.page,
+    pageSize: variables.limit,
+    showSizeChanger: true,
+    showQuickJumper: true,
+    showTotal: total => `共 ${total} 条记录`,
+  };
+
+  const actions = [
+    { name: '刷新', icon: 'reload', action: () => refetch() },
+    // {
+    //   name: '新增',
+    //   icon: 'file-add',
+    //   action: () => router.push(`${PATH}/create`),
+    //   hide: !canCreateAny(AUTH_RESOURCE)
+    // },
+    // {
+    //   name: '删除',
+    //   icon: 'delete',
+    //   action: () => {
+    //     client.mutate({
+    //       mutation: M_DELETE_USER,
+    //       variables: { ids: selectedRows.map(item => item.id).join(',') },
+    //       update: (proxy, { data }) => {
+    //         if (data.deleteUser) {
+    //           message.success('删除成功');
+    //           refetch();
+    //         }
+    //       },
+    //     });
+    //   },
+    //   disabled: selectedRows.length <= 0,
+    //   hide: !canDeleteAny(AUTH_RESOURCE),
+    //   confirm: true,
+    //   confirmTitle: `确定要删除吗?`,
+    // },
+    {
+      name: '导入',
+      icon: 'import',
+      action: () => refetch(),
+      hide: !canCreateAny(AUTH_RESOURCE),
+    },
+    { name: '导出', icon: 'export', action: () => refetch(), hide: !canReadAny(AUTH_RESOURCE) },
+  ];
+
+  return (
+    <Fragment>
+      <PageHeaderWrapper>
+        <StandardConfirm
+          title="请输入驳回理由"
+          visible={visible}
+          setVisible={setVisible}
+          onConfirm={reason => {
+            client.mutate({
+              mutation: M_APPROVAL_USER,
+              variables: {
+                data: {
+                  id: current.id,
+                  status: UserStatusEnum.REJECTED,
+                  reason,
+                },
+              },
+              update: (proxy, { data }) => {
+                if (data.approvalUser) {
+                  message.success('操作成功');
+                  refetch();
+                }
+              },
+            });
+          }}
+        />
+
+        <StandardRow>
+          <Row gutter={16}>
+            <Col lg={6}>
+              <Affix style={{ display: 'inline-block' }} offsetTop={80}>
+                <StandardActions actions={actions} />
+              </Affix>
+            </Col>
+          </Row>
+        </StandardRow>
+
+        <StandardTable
+          loading={loading}
+          dataSource={dataSource}
+          columns={columns}
+          pagination={pagination}
+          defaultFilter={defaultFilter}
+          state={variables}
+          onChange={values => setVariables({ ...values })}
+          onRowSelectionChange={selectedRows => setSelectedRows(selectedRows)}
+        />
+      </PageHeaderWrapper>
+    </Fragment>
+  );
+};
