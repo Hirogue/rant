743bc7047dd04dea9cbfcb496f0bf93555c99285
Hirogue <314367747@qq.com>
2019-08-30 10:50:10 +0800

    update capital's create time

--- service/src/database/subscribers/capital.subscriber.ts
+++ service/src/database/subscribers/capital.subscriber.ts
@@ -18,20 +18,22 @@ export class CapitalSubscriber implements EntitySubscriberInterface<Capital> {
     }
 
     private handleChange(entity: Capital) {
-        entity.summary = entity.info ? textInterception(entity.info, 40) : '';
-
-        entity.hideContact = entity.contact
-            ? entity.contact.substr(0, 1).padEnd(entity.contact.length, '*')
-            : '';
-
-        entity.hidePhone = entity.phone
-            ? entity.phone.replace(entity.phone.substring(3, 7), '****')
-            : '';
-
-        entity.hideCompany = entity.company
-            ? entity.company
-                .substr(entity.company.length - 2, entity.company.length)
-                .padStart(entity.company.length, '*')
-            : '';
+        if (!!entity) {
+            entity.summary = entity.info ? textInterception(entity.info, 40) : '';
+    
+            entity.hideContact = entity.contact
+                ? entity.contact.substr(0, 1).padEnd(entity.contact.length, '*')
+                : '';
+    
+            entity.hidePhone = entity.phone
+                ? entity.phone.replace(entity.phone.substring(3, 7), '****')
+                : '';
+    
+            entity.hideCompany = entity.company
+                ? entity.company
+                    .substr(entity.company.length - 2, entity.company.length)
+                    .padStart(entity.company.length, '*')
+                : '';
+        }
     }
 }
