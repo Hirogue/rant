9469ece9d19acc6398b85790c10312fe4f69d3e6
roylin <1002591652@qq.com>
2019-10-11 10:11:40 +0800

    improvement: logger module

--- libs/logger/src/index.ts
+++ libs/logger/src/index.ts
@@ -1,6 +1,5 @@
 export * from './logger-format';
 export * from './logger-level.enum';
-export * from './logger.constants';
 export * from './logger.middleware';
 export * from './logger.module';
 export * from './logger.service';
