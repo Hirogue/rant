a5a3891cef6e4695742e13b1d760ad8967ba8fa9
roylin <1002591652@qq.com>
2019-10-12 18:22:08 +0800

    improvement: queue constants

--- libs/queue/src/queue.constants.ts
+++ libs/queue/src/queue.constants.ts
@@ -1,3 +1,3 @@
-export const BULL_MODULE_QUEUE = '__bull_module_queue';
-export const BULL_MODULE_QUEUE_PROCESS = '__bull_module_queue_process';
-export const BULL_MODULE_ON_QUEUE_EVENT = '__bull_module_on_queue_event';
+export const BULL_MODULE_QUEUE = Symbol('BULL_MODULE_QUEUE');
+export const BULL_MODULE_QUEUE_PROCESS = Symbol('BULL_MODULE_QUEUE_PROCESS');
+export const BULL_MODULE_ON_QUEUE_EVENT = Symbol('BULL_MODULE_ON_QUEUE_EVENT');
